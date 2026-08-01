326a939ee8edfb82fcaa760695dff7428635cba7
Sujai <sujaiit0696@gmail.com>
2026-08-01 16:34:26 +0530

    Add professional portal styling: website CSS loading, page transitions, form enhancements, button styles, micro-interactions, footer, alerts, badges, tables

--- team_update_tool/hooks.py
+++ team_update_tool/hooks.py
@@ -10,6 +10,7 @@ app_color = "#2E8B57"
 required_apps = ["erpnext"]
 
 app_include_css = "/assets/team_update_tool/css/team_update_tool.css"
+website_include_css = "/assets/team_update_tool/css/team_update_tool.css"
 website_include_js = "/assets/team_update_tool/js/team_update_tool.js"
 
 after_install = "team_update_tool.install.after_install"
